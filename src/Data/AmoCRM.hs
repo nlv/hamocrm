8da5078be0c2d1e656571fbb3b38b660f29c531c
Leon Nikitin <nlv@lab321.ru>
2022-05-13 18:01:35 +0600

    Первый работающи вариант, который выбирает порцию заявок и выводит ид и название на консоль

---
+++ src/Data/AmoCRM.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Data.AmoCRM ( 
+    Lead(..)
+    ,ListFromAmoCRM(..)
+    ,parseListFromAmoCRM
+    ,parseLeadFromAmoCRM
+    ,lid
+    ,lname
+) where
+
+import GHC.Generics
+import Data.Time
+import Data.Text
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Aeson.Key
+import Data.Vector (toList)
+import Control.Lens
+
+import Control.Monad
+
+
+data Lead = Lead {
+  _lid           :: Integer
+, _lname         :: Text
+-- , _lresponsible  :: Text
+-- , _laddress      :: Text
+-- , _ldateVisit    :: UTCTime
+-- , _ltype         :: Text
+-- , _lsellCost     :: Float -- Цена клиенту
+-- , _lpartsCost    :: Float -- Стоимость материалов
+-- , _lworksCost    :: Float -- Стоимость работ
+-- , _lofficeIncome :: Float -- Перевод в офис
+-- , _lclosedDate   :: UTCTime -- Дата закрытия
+-- , _lstatusId     :: Text
+} deriving (Generic, Show)
+
+data ListFromAmoCRM a = ListFromAmoCRM {
+  _els :: [a]
+} deriving (Generic, Show)
+
+makeLenses ''Lead
+makeLenses ''ListFromAmoCRM
+
+parseListFromAmoCRM :: String -> (Value -> Parser a) -> Value -> Parser (ListFromAmoCRM a)
+parseListFromAmoCRM ename parseElement = 
+  withObject "List from amoCRM" $ \obj -> do
+    embedded <- (obj .: "_embedded")
+    els <- embedded .: (fromString ename)
+    ListFromAmoCRM <$> toList <$> withArray "list of elements" (mapM parseElement) els
+
+parseLeadFromAmoCRM :: Value -> Parser Lead
+parseLeadFromAmoCRM = 
+  withObject "lead" $ \obj -> do
+    Lead <$> obj .: "id" <*> obj .: "name"
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,54 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
 
+
+import           Data.Aeson
+import           Data.Aeson.Key
+import           Data.Aeson.Types
+import           Data.Aeson.Parser           (json)
+import           Data.Conduit                (($$))
+import           Data.Conduit.Attoparsec     (sinkParser)
+import           Network.HTTP.Client         
+import           Network.HTTP.Client.Conduit (bodyReaderSource)
+import           Network.HTTP.Client.TLS     (tlsManagerSettings)
+import           Network.HTTP.Types.Status   (statusCode)
+
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.ByteString as B
+
+import Control.Applicative
+import Control.Monad
+import Control.Lens
+
+import Data.Text (unpack)
+
+import Data.AmoCRM
+
+user = "artandem7"
+
+token :: B.ByteString
+token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImI3YTU2MmUxNWRiMmJjYjZhYjNlNWQ3ZDA5ZjgxNTQ2ZGNmZGQ0ZDMzMjI1MjNhMTYyMWM1NmI3YjAyN2M2MTNkMWJmMzgwNWNhYjcxZDBmIn0.eyJhdWQiOiI1N2ZlZjgyNy1hNDNlLTQ2NGQtOGU0My1mYzQyOGJjMzBlMmQiLCJqdGkiOiJiN2E1NjJlMTVkYjJiY2I2YWIzZTVkN2QwOWY4MTU0NmRjZmRkNGQzMzIyNTIzYTE2MjFjNTZiN2IwMjdjNjEzZDFiZjM4MDVjYWI3MWQwZiIsImlhdCI6MTY1MjQxODc3MSwibmJmIjoxNjUyNDE4NzcxLCJleHAiOjE2NTI1MDUxNzEsInN1YiI6Ijc4Nzk1OTEiLCJhY2NvdW50X2lkIjoyOTM3ODExOSwic2NvcGVzIjpbImNybSJdfQ.QzwZqA9QP94yfcGSKNgK1ots8vgGK0sT8owqPRXdDks1mOb-NcIq8je1Jo_US6S8FifVlPlym26hUF7gMp0DkRgWLxkA8D2eP2314Rcwq5eqgTifzEyDL1jym6EkXjklpbMf3NYqptFVw5OhvlHCBofzkEZIpA8S-ouokM38Z0lxZx3WfpSASyuMXjzzj4cZM7R7ZGkFcwtb_AbqynTve6JEO6AZbzRF0DmU91bJmNLhMvI26yAgVwmxwEb5VG3r96mlIOABBhRbBnQI0dgY7C1Sm36tb6k-0D70EcLVNOgvhnNlAXWyJtUiunIvr91PXUqBL4uWDdHLc5bFD3i_dg"
+
+request' = applyBearerAuth token $ parseRequest_ $ "https://" ++ user ++ ".amocrm.ru/api/v4/leads" 
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    manager <- newManager tlsManagerSettings
+    request <- applyBearerAuth token <$> parseRequest ("https://" ++ user ++ ".amocrm.ru/api/v4/leads")
+
+    -- request2 <- applyBearerAuth token <$> parseRequest ("https://" ++ user ++ ".amocrm.ru/api/v4/users")
+
+    withResponse request manager $ \response -> do
+        putStrLn $ "The status code was: " ++
+                   show (statusCode $ responseStatus response)
+
+        v <- bodyReaderSource (responseBody response) $$ sinkParser json
+
+        let a = (parseEither (parseListFromAmoCRM "leads" parseLeadFromAmoCRM) v) :: Either String (ListFromAmoCRM Lead)
+        case a of
+            Right leads -> mapM_ putStrLn $ map (\e -> show (e ^. lid) ++ " ;;; " ++ unpack (e ^. lname)) $ _els leads
+            Left err -> putStrLn $ show err
+
