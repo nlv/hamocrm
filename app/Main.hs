56e5c0ff0d3ffc347caac4313c9180bf08787457
Leon Nikitin <nlv@lab321.ru>
2022-05-22 15:42:08 +0600

    Попытка реализовать обнолвление токена

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 
 module Main where
 
--- import Control.Monad.Trans.Except
--- import Control.Monad
+import GHC.Generics
+import Control.Monad.IO.Class
 import Data.Text
+import Data.Time
+import Data.Time.Clock.POSIX
 import Data.ByteString as B
--- import Data.String
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Char8 as BS
--- import qualified Data.Text.Encoding.Base64 as B64
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Wai.Middleware.Cors
@@ -16,40 +19,91 @@ import Network.Wai.Middleware.Servant.Options
 import Network.HTTP.Types.Method
 import Servant
 import System.IO
+import Control.Monad.Trans.Except
+
+
 
 import Options.Applicative
 import Data.Semigroup ((<>))
 
+import Data.Aeson
+
 import Api
+import Network.Amocrm
+
+
 
 tokenFile = "token.txt"
 
-data Options = Options {
-    optUser :: String
-  , optPort :: Int
+data ProgOptions = ProgOptions {
+    optUser         :: String
+  , optPort         :: Int
+  , optClientId     :: String
+  , optClientSecret :: String
+  , optRedirectUri  :: String
   }
+
+instance TokenRequest ProgOptions where
+  tokUser         = optUser
+  tokClientId     = optClientId
+  tokClientSecret = optClientSecret
+  tokRedirectURI  = optRedirectUri
+
 
 api :: Proxy Api
 api = Proxy
 
-optParser :: Parser Options
-optParser = Options
-        <$> strOption   ( long "user" <> short 'u' <> help "AmoCRM user")
-        <*> option auto ( long "port" <> short 'p' <> help "Service port")
+optParser :: Parser ProgOptions
+optParser = ProgOptions
+        <$> strOption   ( long "user"     <> short 'u' <> help "AmoCRM user")
+        <*> option auto ( long "port"     <> short 'p' <> help "Service port")
+        <*> strOption   ( long "id"       <> short 'i' <> help "Client id")
+        <*> strOption   ( long "secret"   <> short 's' <> help "Client secret")
+        <*> strOption   ( long "redirect" <> short 'r' <> help "Redirect URI")
 
 main :: IO ()
 main = do
-    h <- openFile tokenFile ReadMode 
-    token <- BS.hGetLine h
-    hClose h
     opts <- execParser opts'
-    runIt opts token
+    tokens' <- getAccessToken opts
+    case tokens' of
+      Left err -> System.IO.putStrLn err
+      Right access_token -> runIt opts access_token
     where opts' = info (optParser <**> helper)
                     ( fullDesc
                     <> progDesc "AmoCRM integration service"
                     <> header "AmoCRM integration service")
 
-runIt :: Options -> ByteString -> IO ()
+
+getAccessToken :: ProgOptions -> IO (Either String ByteString)
+getAccessToken opts = do
+    h <- liftIO $ openFile tokenFile ReadMode 
+    tokens' <- eitherDecodeStrict <$> B.hGetLine h :: IO (Either String OAuth2)
+    liftIO $ hClose h
+    case tokens' of
+      Left err -> pure $ Left err
+      Right tokens -> do
+        endTime <- pure $ ( fromInteger . endTokenTime ) tokens
+        now <- getPOSIXTime
+        liftIO $ Prelude.putStrLn $ show now
+        liftIO $ Prelude.putStrLn $ show endTime
+        let expired = now > endTime
+        if expired 
+          then do
+              liftIO $ System.IO.putStrLn $ "Надо менять"  
+              newToken' <- refreshToken opts tokens
+              liftIO $ Prelude.putStrLn $ show newToken'
+              case newToken' of
+                Left err -> pure $ Left err
+                Right newToken -> do
+                  h <- liftIO $ openFile tokenFile WriteMode 
+                  liftIO $ BS.hPutStrLn h $ BL.toStrict $ encode newToken
+                  liftIO $ hClose h
+                  pure $ Right $ BS.pack $ access_token newToken
+          else
+            pure $ Right $ BS.pack $ access_token tokens
+
+   
+runIt :: ProgOptions -> ByteString -> IO ()
 runIt opts token = do
   let port = optPort opts
       settings =
@@ -62,9 +116,5 @@ mkApp :: String -> ByteString -> IO Application
 mkApp user token = return $ cors (const $ Just policy) $ provideOptions api $ serve api (server user token)
   where
     policy = simpleCorsResourcePolicy { corsRequestHeaders = [ "content-type" ], corsMethods = [methodGet, methodPost, methodDelete, methodOptions] }
-
-
-
-
 
 
--- src/Network/Amocrm.hs
+++ src/Network/Amocrm.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Network.Amocrm
     ( getList
+    , TokenRequest(..)
+    ,OAuth2(..)
+    , refreshToken
     ) where
 
-
+import GHC.Generics
 import           Data.Aeson
 import           Data.Aeson.Key
 import           Data.Aeson.Types
@@ -23,13 +27,30 @@ import qualified Data.ByteString.Char8 as BC
 import Control.Applicative
 import Control.Monad
 import Control.Monad.IO.Class
-import Control.Lens
+import Control.Lens hiding ((.=))
 
 import Data.Text (Text, unpack)
 import Data.ByteString
 import Data.Vector (toList)
 
 import Data.Amocrm
+
+data OAuth2 = OAuth2 {
+    access_token  :: String
+  , refresh_token :: String
+
+  , endTokenTime  :: Integer
+} deriving (Generic, Show)
+
+class TokenRequest a where
+  tokUser      :: a -> String
+  tokClientId     :: a -> String
+  tokClientSecret :: a -> String
+  tokRedirectURI  :: a -> String
+
+
+instance FromJSON OAuth2
+instance ToJSON OAuth2
 
 getList :: AmocrmModule a => String -> String -> ByteString -> IO (Either String (ListFromAmocrm a))
 getList ename user token = do
@@ -39,6 +60,9 @@ getList ename user token = do
 
     withResponse request manager $ \response -> do
       v <- bodyReaderSource (responseBody response) $$ sinkParser json
+      liftIO $ Prelude.putStrLn $ "====="
+      liftIO $ Prelude.putStrLn $ show v
+      liftIO $ Prelude.putStrLn $ "====="
       pure $ parseEither (parseList ename) v
 
 
@@ -53,3 +77,33 @@ parseList  ename =
     parseElement :: AmocrmModule a => Value -> Parser a
     parseElement = 
       withObject "element" parser
+
+
+refreshToken :: TokenRequest a => a -> OAuth2 -> IO (Either String OAuth2)
+refreshToken tok oauth = do
+    let requestObject = 
+          object 
+            [ "client_id" .= (tokClientId tok)
+            , "client_secret" .= (tokClientSecret tok)
+            , "grant_type" .= ("refresh_token" :: String)
+            , "redirect_uri" .= (tokRedirectURI tok)
+            , "refresh_token" .= (refresh_token oauth)
+            ]
+     
+    liftIO $ LB.putStrLn $ encode requestObject
+    manager <- liftIO $ newManager tlsManagerSettings
+    request' <- parseRequest ("https://" ++ tokUser tok ++ ".amocrm.ru/oauth2/access_token")
+    let request = request' { method = "POST", requestBody = RequestBodyBS $ LB.toStrict $ encode requestObject }
+    -- let r = setRequestBody request "bbb"
+    let RequestBodyBS a = requestBody request
+
+    liftIO $ B.putStrLn a
+
+    liftIO $ Prelude.putStrLn $ show request
+
+    withResponse request manager $ \response -> do
+      v <- bodyReaderSource (responseBody response) $$ sinkParser json
+      -- let r = parseEither parse v 
+      LB.putStrLn $ encode v
+      pure $ parseEither parseJSON v
+
