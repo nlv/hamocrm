44ce2c5b56ce3b3daa8d2d6dc8e2edad9e18104c
Leon Nikitin <nlv@lab321.ru>
2022-05-30 22:33:42 +0600

    Начали переделывать на кастомный стек монад - конфигурация на чтение, токен на запись, обновляемый через IO

--- src/Api.hs
+++ src/Api.hs
@@ -12,6 +12,7 @@ import Servant
 
 import Data.ByteString
 import Control.Lens
+import Control.Monad.Reader
 
 import Data.Amocrm
 import Network.Amocrm
@@ -28,17 +29,40 @@ type UsersApi =
           Get '[JSON] [User]
      )      
 
+data ServerReader = ServerReader {
+       srUser  :: String
+     , srToken :: ByteString
+}
+
+serverT :: ServerT Api (ReaderT ServerReader IO)
+serverT = getAny "leads" :<|> getAny "users"
+
+-- server :: String -> ByteString -> ServerT Api (ReaderT ServerReader IO)
 server :: String -> ByteString -> Server Api
-server user token = getAny "leads" user token :<|> getAny "users" user token
+server user token = hoistServer api (readerToHandler user token) serverT
+
+api :: Proxy Api
+api = Proxy
 
 
-getAny :: (AmocrmModule a) => String -> String -> ByteString -> Handler [a]
-getAny mod user token = do
+-- getAny :: (AmocrmModule a) => String -> (ReaderT ServerReader (ExceptT ServerError IO)) [a]
+getAny :: (AmocrmModule a) => String -> (ReaderT ServerReader IO) [a]
+getAny mod = do
+     user <- asks srUser
+     token <- asks srToken
      leads' <- liftIO $ getList mod user token
      case leads' of
           Right leads -> pure $ leads ^. els
           Left err -> do
                liftIO $ Prelude.putStrLn $ show err
-               throwError err404          
+               error $ show err
+               -- lift $ throwError err404
+               -- pure [] --err404          
+
+
+readerToHandler :: String -> ByteString -> ReaderT ServerReader IO a -> Handler a
+readerToHandler user token r = do
+     a <- liftIO (runReaderT r ServerReader {srUser = user, srToken = token})
+     pure a
 
 
